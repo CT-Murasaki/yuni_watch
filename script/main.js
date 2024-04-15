27c958951ab4c1b2b407b32ef914e6cc22824c88
RS☆murasaki <rintam65@gmail.com>
2024-04-15 20:14:16 +0900

    変数周りを修正

--- script/main.js
+++ script/main.js
@@ -7,141 +7,17 @@ function main(param) {
 }
 
 function makescene(param) {
+    let mainList = []
+    let imagenum = 126;
+    for (let i = 1; i <= imagenum; i++){
+        mainList.push("main" + i);
+    }
+    mainList.push("se_finish");
 
-    var scene = new g.Scene({
+    let scene = new g.Scene({
         game: g.game,
         // このシーンで利用するアセットのIDを列挙し、シーンに通知します
-        assetIds: [
-            "main1",
-            "main2",
-            "main3",
-            "main4",
-            "main5",
-            "main6",
-            "main7",
-            "main8",
-            "main9",
-            "main10",
-            "main11",
-            "main12",
-            "main13",
-            "main14",
-            "main15",
-            "main16",
-            "main17",
-            "main18",
-            "main19",
-            "main20",
-            "main21",
-            "main22",
-            "main23",
-            "main24",
-            "main25",
-            "main26",
-            "main27",
-            "main28",
-            "main29",
-            "main30",
-            "main31",
-            "main32",
-            "main33",
-            "main34",
-            "main35",
-            "main36",
-            "main37",
-            "main38",
-            "main39",
-            "main40",
-            "main41",
-            "main42",
-            "main43",
-            "main44",
-            "main45",
-            "main46",
-            "main47",
-            "main48",
-            "main49",
-            "main50",
-            "main51",
-            "main52",
-            "main53",
-            "main54",
-            "main55",
-            "main56",
-            "main57",
-            "main58",
-            "main59",
-            "main60",
-            "main61",
-            "main62",
-            "main63",
-            "main64",
-            "main65",
-            "main66",
-            "main67",
-            "main68",
-            "main69",
-            "main70",
-            "main71",
-            "main72",
-            "main73",
-            "main74",
-            "main75",
-            "main76",
-            "main77",
-            "main78",
-            "main79",
-            "main80",
-            "main81",
-            "main82",
-            "main83",
-            "main84",
-            "main85",
-            "main86",
-            "main87",
-            "main88",
-            "main89",
-            "main90",
-            "main91",
-            "main92",
-            "main93",
-            "main94",
-            "main95",
-            "main96",
-            "main97",
-            "main98",
-            "main99",
-            "main100",
-            "main101",
-            "main102",
-            "main103",
-            "main104",
-            "main105",
-            "main106",
-            "main107",
-            "main108",
-            "main109",
-            "main110",
-            "main111",
-            "main112",
-            "main113",
-            "main114",
-            "main115",
-            "main116",
-            "main117",
-            "main118",
-            "main119",
-            "main120",
-            "main121",
-            "main122",
-            "main123",
-            "main124",
-            "main125",
-            "main126",
-            
-			//音声ファイルはhttps://github.com/akashic-contents　CC BY 2.1 JP　DWANGO Co., Ltd.
-            "se_finish"
-	    ]
+        assetIds: mainList
     });
 
     // 市場コンテンツのランキングモードでは、g.game.vars.gameState.score の値をスコアとして扱います
@@ -150,13 +26,13 @@ function makescene(param) {
     scene.onLoad.add(function () {
         // ここからゲーム内容を記述します
 
-        var font = new g.DynamicFont({
+        let font = new g.DynamicFont({
             game: g.game,
             fontFamily: "sans-serif",
             size: 48
         });
         // スコア表示用のラベル
-        var scoreLabel = new g.Label({
+        let scoreLabel = new g.Label({
             scene: scene,
             text: "SCORE: 0",
             font: font,
@@ -164,7 +40,7 @@ function makescene(param) {
             textColor: "white"
         });
         scene.append(scoreLabel);
-        var closeingLabel = new g.Label({
+        let closeingLabel = new g.Label({
             scene: scene,
             text: "口が閉じた回数: 0",
             font: font,
@@ -174,7 +50,7 @@ function makescene(param) {
         });
         scene.append(closeingLabel);
 
-        let imagenum = 126;
+        let score = 0;
         let closeingcnt = 0;
         let images = [];
         for (let i = 1; i <= imagenum; i++) {
@@ -210,24 +86,24 @@ function makescene(param) {
                     case 122:
                     case 123:
                     case 125:
-                        g.game.vars.gameState.score += g.game.random.get(0, 10000);
+                        score += g.game.random.get(0, 10000);
                         closeingcnt += 1;
                         break;
 
                     case 74:
                     case 100:
-                        g.game.vars.gameState.score += 10000;
+                        score += 10000;
                         break;
 
                     case 104:
-                        g.game.vars.gameState.score += 100000;
+                        score += 100000;
                         closeingcnt += 1;
                         break;
 
                     default:
-                        g.game.vars.gameState.score += g.game.random.get(0, 10000);
+                        score += g.game.random.get(0, 10000);
                 }
-                scoreLabel.text = "SCORE: " + g.game.vars.gameState.score;
+                scoreLabel.text = "SCORE: " + score;
                 scoreLabel.invalidate();
                 closeingLabel.text = "口が閉じた回数: " + closeingcnt;
                 closeingLabel.invalidate();
@@ -248,7 +124,7 @@ function makescene(param) {
                     images[i].invalidate();
                 }
                 if (correction == false){
-                    g.game.vars.gameState.score = Math.ceil(g.game.vars.gameState.score + (g.game.vars.gameState.score * closeingcnt * 0.01));
+                    g.game.vars.gameState.score = Math.ceil(score + (score * closeingcnt * 0.01));
                     correction = true;
                 }
                 
